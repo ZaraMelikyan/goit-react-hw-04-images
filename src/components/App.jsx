76190285eab80ee55c58157cf299a1845d6f7ca0
ZaraMelikyan <zaramelikyan1@gmail.com>
2023-02-04 21:08:52 +0300

    App fix
    App fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect} from 'react';
 import { Button, ImageGallery, Loader, Searchbar } from 'components/index';
 import { getImages } from 'services/ApiService';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -10,59 +10,30 @@ export function App() {
   const [totalPage, setTotalPage] = useState(1);
   const [status, setStatus] = useState('idle');
 
-  const intervalId = useRef(null);
-
-useEffect(() => {
+  useEffect(() => {
+  
+    if (query !== "") {    
+      makeNewFetch();
+    }
 
     async function makeNewFetch () {
     try {
       const response = await getImages(query, page);
       const { totalHits, hits } = response;
 
-      if (totalHits === 0) {
-        setTotalPage(1);
+      if (totalHits === 0) {        
         setStatus('rejected');
         Notify.failure('Sorry, there are no images matching your search request. Please try another request.');
         return;
-      }
-
+      }      
       setTotalPage(Math.ceil(totalHits / 12));
-      setImages(hits);
-      setStatus('resolved');
-      Notify.success(`Hurray! ${totalHits} images found`);
+      setImages(images => [...images, ...hits]);
+      setStatus('resolved');      
 
     } catch (error) {
       setStatus('rejected');
       console.log(error.message);
     }
-    }
-  
-
-async function loadMore () {
-    try {
-      const response = await  getImages(query, page);
-      const newImages = response.hits;
-      setImages(images => [...images, ...newImages]);
-      setStatus('resolved');
-      intervalId.current = setTimeout(() => scroll(), 100);
-
-      } catch (error) {
-        setStatus('rejected');
-        console.log(error);
-      }
-    }
-  if (page === 1 && query !== "") {
-      setStatus('pending');
-      makeNewFetch();
-    }
-    
-    if (page > 1) {
-      setStatus('pending');
-      loadMore();
-    }
-
-    return () => {
-      clearInterval(intervalId.current);
     }
   }, [query, page]);
 
@@ -72,30 +43,22 @@ async function loadMore () {
     const onSubmit = submitQuery => {
     if (submitQuery.trim() !== '' && submitQuery !== query) {
       setStatus('idle');
-      setQuery(submitQuery);
-      setPage(1);
-      setImages([]);
+      setQuery(submitQuery);      
     }
     if (submitQuery.trim() === '') {
       setStatus('emptyQuery');
-      setQuery('');
+      setQuery('');     
+      }
       setImages([]);
       setPage(1);
-    }
+      setTotalPage(1);
   };
 
   const handleBtnClick = () => {
     setPage(page => page + 1);
   }
 
-  const scroll = () => {
-    const { clientHeight } = document.documentElement;
-    window.scrollBy({
-      top: clientHeight - 180,
-      behavior: 'smooth',
-    });
-  };
-
+ 
     return (
       <>
         <Searchbar onSubmit={onSubmit} isSubmitting={status === 'pending'} />
@@ -109,18 +72,3 @@ async function loadMore () {
 
 
 
-
-  //async componentDidUpdate(_, prevState) {
-
- //   const { query, page } = this.state;
-
- //   if (prevState.page !== page && page !== 1) {
-//      this.setState({ status: 'pending' });
-//      this.loadMore(query, page);
-//    }
-
-//    if (prevState.query !== query && query !== "") {
- //     this.setState({ status: 'pending' });
-//      this.makeNewFetch(query, page)
-//    }
-//  }
